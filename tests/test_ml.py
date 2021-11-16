7795f52a6e90e6519e1445dc68a64ea0bebe8c94
wilcox <rongunowilcox@gmail.com>
2021-11-16 17:50:56 +1100

    Enhance tests

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -4,6 +4,6 @@ def test_index_get(client):
     response = client.get('/')
     assert response.status_code == 200
 def test_prediction(client):
-    response = client.post('/result', json={'Pregnancies': '6', 'Glucose': '148', 'BloodPressure': '72', 'SkinThickness': '35', 'Insulin': '0', 'BMI': '33.6', 'DiabetesPedigreeFunction': '0.627', 'Age': '50'})
-    data = response.get_json()
-    #assert data['prediction'] == 1
+    response = client.post('/result', data={'Pregnancies': '6', 'Glucose': '148', 'BloodPressure': '72', 'SkinThickness': '35', 'Insulin': '0', 'BMI': '33.6', 'DiabetesPedigreeFunction': '0.627', 'Age': '50'})
+    resp_data = response.get_json()
+    assert resp_data['prediction'] == 1
--- wsgi.py
+++ wsgi.py
@@ -12,35 +12,32 @@ def index():
     return render_template('index.html')
 # the predictor function
 def predictor(data):
-    # print(data)
+    # load the serializzed model
     loaded_model = pickle.load(open("model.pkl","rb"))
+    # make the prediction
     result = loaded_model.predict(data)
     return result[0]
 @app.route("/result", methods=['POST'])
 def result():
     if request.method == 'POST':
         to_predict_list = request.form.to_dict()
-        #to_predict_list = list(to_predict_list.values())
-        #to_predict_list = list(map(int, to_predict_list))
-        # result = predictor(to_predict_list)
-        #print(list(to_predict_list.values()))
-        # df3 = pd.Series(to_predict_list)
-        # df3 = pd.DataFrame(df3)
-        # df3.swapaxes("columns","index")
-        # print(df3)
+      
         # TODO Refactor this
         print(to_predict_list)
         # create a list
         values = list(to_predict_list.values())
         #create a dataframe
         df2 = pd.DataFrame(np.array([values]),columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'])
-        #print(df2)
-        #error handlng
+        
+        # basic error handling
         if df2 is None:
             result = -1
         else:
             result=predictor(df2)
-    #return render_template('results.html', prediction=result)
-    return jsonify(prediction=int(result))
+        
+        #return render_template('results.html', prediction=result)
+        return jsonify(prediction=int(result))
+    else:
+        return {"error": "POST is only allowed"}
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
